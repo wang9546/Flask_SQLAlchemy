b8186342220286d2ef30a44c76ccc55c3616f57d
wang9546 <954605446@qq.com>
2019-06-27 15:00:43 +0800

    update fires

--- orm.py
+++ orm.py
@@ -1,10 +1,92 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+class Flask_sqlalchemt_one():
+    Integer, String =Integer,String
+    def __init__(self,app=None):
+        self.app = app
+        self.Model = self.make_declarative_base()
+        self.Column = Column
+        if app is not None:
+            self.init_app(app)
 
-from flask_sqlalchemy import SQLAlchemy
-Base = declarative_base()
+    def make_declarative_base(self):
+        '''
+        Base = declarative_base()
+        # 每一次在创建数据表的时候都要做这样一件事
+        :return:
+        '''
+        self.base = declarative_base()
+        return self.base
 
 
-class User(Base):
-    __tablename__ = "user"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    def init_app(self,app):
+        '''
+        初始化
+        :param app:
+        :return:
+        '''
+        database_url = app.config.setdefault('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://root:@127.0.0.1:3306/homework?charset=utf8')
+        poll_size = app.config.setdefault('SQLALCHEMY_POOL_SIZE', None)
+        print(poll_size)
+        poll_timeout = app.config.setdefault('SQLALCHEMY_POOL_TIMEOUT', None)
+        track_modifications = app.config.setdefault(
+            'SQLALCHEMY_TRACK_MODIFICATIONS', None
+        )
+        self.engine = self.create_engines(database_url,
+                                          max_overflow=0,
+                                          pool_size=poll_size,
+                                          pool_timeout=poll_timeout,
+                                          )
+        self.session = self.make_session()
+        self.base.query = self.session.query
+        @app.teardown_appcontext
+        def func(cc):
+            '''
+            每次请求结束后调用
+            :param cc:
+            :return:
+            '''
+            print(11111)
+
+    def create_engines(self,database_url,max_overflow=None,pool_size=None,pool_timeout=None):
+        '''
+        创建数据库引擎
+        :param database_url:
+        :param max_overflow:
+        :param pool_size:
+        :param pool_timeout:
+        :return:
+        '''
+        engine = create_engine(
+            database_url,
+            max_overflow=max_overflow,
+            pool_size=pool_size,
+            pool_timeout=pool_timeout,
+        )
+        return engine
+
+    def make_session(self):
+        '''
+        session会话
+        :return:
+        '''
+        Session = sessionmaker(self.engine)
+        db_sesson = scoped_session(Session)
+        return db_sesson
+
+    def create_all(self):
+        self.Model.metadata.create_all(self.engine)
+        return
+
+db = Flask_sqlalchemt_one()
+
+class User(db.Model):
+    '''
+    model表
+    '''
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    name = db.Column(db.String(16),nullable=False)
+
--- web.py
+++ web.py
@@ -1,19 +1,31 @@
-from flask import Flask
-from orm import User
-import os
-app = Flask(__name__)
-base_dir = '/'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
-app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+from flask import Flask,Blueprint, request, render_template,jsonify
 
-db = User(app)
+from orm import db,User
+user = Blueprint("user", __name__)
+def create_app():
+    app = Flask(__name__)
+    # 初始化App配置专门针对 SQLAlchemy 进行配置
+    # SQLALCHEMY_DATABASE_URI 配置 SQLAlchemy 的链接字符串儿
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@127.0.0.1:3306/homework?charset=utf8'
+    # SQLALCHEMY_POOL_SIZE 配置 SQLAlchemy 的连接池大小
+    app.config["SQLALCHEMY_POOL_SIZE"] = 5
+    # SQLALCHEMY_POOL_TIMEOUT 配置 SQLAlchemy 的连接超时时间
+    app.config["SQLALCHEMY_POOL_TIMEOUT"] = 15
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # 初始化SQLAlchemy , 本质就是将以上的配置读取出来
+    db.init_app(app)
+    app.register_blueprint(user)
+    return app
 
-
-
-
-@app.route("/")
+@user.route("/")
 def index():
-    return "hello S17"
+    # db.create_all()
+    db.session.add(User(name='111')) # 添加
+    db.session.commit()
+    user_info = User.query(User).filter(User.name == 'name').first() #查询
+    print(user_info)
+    return jsonify({'user':user_info.name})
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
+    app.run(processes=10)
